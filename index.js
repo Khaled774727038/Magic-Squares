54d901506e841dcc9fe6678ea3e9ee87b911e27e
[Khaled Al-Hammadi] <[khaled Al-Hammadi]>
2024-02-20 16:59:08 +0100

    Magic-Squares

---
+++ index.js
@@ -0,0 +1,15 @@
+let arr=[
+    8,1,6,
+    3,5,7,
+    4,9,2
+  ];
+  
+  let diter1 = arr[0] +arr[4]+arr[8];
+  let diter2 = arr[2] +arr[4]+arr[6];
+  if (diter1 === diter2) {
+    console.log(true);
+  }
+  eles 
+  {
+     console.log(false);
+  }
